1bc79c2d53c2f633c1642e2f24e2106670b9755e
DustyC <123617229+DustyCs@users.noreply.github.com>
2025-10-14 20:07:57 +0800

    feat: finished deployment, services, pods on k8s moving to ci/cd

---
+++ k8s-web-to-nginx/index.mjs
@@ -0,0 +1,21 @@
+import express from 'express';
+import fetch from 'node-fetch';
+import os from 'os';
+
+const app = express();
+const PORT = 3000
+
+app.get('/', (req, res) => {
+    console.log(`Request served by ${os.hostname()}, Hello World!`);
+    res.send(`Hello from ${os.hostname()}\n`);
+});
+
+app.get("/nginx", async (req, res) => {
+    const response = await fetch('http://nginx');
+    const body = await response.text();
+    res.send(`Response from Nginx (via ${os.hostname()}):\n\n${body}`);
+});
+
+app.listen(PORT, () => {
+    console.log(`Web Server is running on port ${PORT}`);
+});
